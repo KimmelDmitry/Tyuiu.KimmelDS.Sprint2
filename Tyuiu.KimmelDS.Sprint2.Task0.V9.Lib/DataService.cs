ff4ea817d16ffe18944f05c389003d8c7c4e88bc
EvilHui <Ukrumoski@gmail.com>
2024-10-23 16:57:01 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KimmelDS.Sprint2.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KimmelDS.Sprint2.Task0.V9.Lib
+{
+    public class DataService : ISprint2Task0V9
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            return new bool[] { x == y, x != y, x < y, x > y, x <= y, x >= y };
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint2.Task0.V9/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint2.Task0.V9.Lib;
+namespace Tyuiu.KimmelDS.Sprint2.Task0.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #9                                                            *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetCompareOperations(1054, 375);
+            Console.WriteLine(string.Join(", ", result.Select(x => x)));
+            Console.ReadKey();
+        }
+    }
+}
