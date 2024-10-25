1e8a648aaefc0c12e8b5d2fce7238d06d942189a
EvilHui <Ukrumoski@gmail.com>
2024-10-25 22:00:09 +0500

    task 3

--- Tyuiu.KimmelDS.Sprint2.Task1.V17.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint2.Task1.V17.Lib/DataService.cs
@@ -6,11 +6,6 @@ namespace Tbuiu.KimmelDS.Sprint2.Task1.V17.Lib
     {
         public bool[] GetLogicOperations(int a, int b, int c, int d)
             => [!(a == b), a == b, !(a < b), !(d > c), d <= c, (d >= c)];
-        //my res
-        //True, False, True, True, True, False
-        //but need
-        //
-        //Ожидалось: [true, false, true, true, true, false]
-        //Получено: [false, true, true, false, false, true]
+        
     }
 }
---
+++ Tyuiu.KimmelDS.Sprint2.Task2.V26.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KimmelDS.Sprint2.Task2.V26.Lib
+{
+    public class DataService : ISprint2Task2V26
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            if ((x >= 2 && x <= 5 && y == 2))
+                return true;
+
+            if ((x >= 2 && x <= 8 && y == 3))
+                return true;
+
+            if ((x >= 2 && x <= 6 && y == 4))
+                return true;
+
+            if ((x >= 5 && x <= 7 && y == 5))
+                return true;
+
+            if ((x >= 4 && x <= 7 && y == 6))
+                return true;
+
+            if ((x >= 5 && x <= 6 && y == 7))
+                return true;
+
+            if ((x >= 4 && x <= 10 && y == 8))
+                return true;
+
+            if ((x >= 7 && x <= 8 && y == 9))
+                return true;
+
+            if ((x >= 7 && x <= 9 && y == 10))
+                return true;
+
+            return false;
+        }
+    }
+}
