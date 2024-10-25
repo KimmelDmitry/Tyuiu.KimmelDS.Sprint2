6fc0d31727a241319e97b61c40ac015ba6fc692d
EvilHui <Ukrumoski@gmail.com>
2024-10-25 21:30:00 +0500

    task ?

--- Tyuiu.KimmelDS.Sprint2.Task1.V17.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint2.Task1.V17.Lib/DataService.cs
@@ -5,6 +5,6 @@ namespace Tbuiu.KimmelDS.Sprint2.Task1.V17.Lib
     public class DataService : ISprint2Task1V17
     {
         public bool[] GetLogicOperations(int a, int b, int c, int d)
-            => [a == b, a != b, !(a < b), d > c, !(d <= c), !(d >= c)];
+            => [!(a == b), a == b, !(a < b), !(d > c), d <= c, (d >= c)];
     }
 }
