f0d60469a8c7d0f8b05a809b5bbba84784409dfa
EvilHui <Ukrumoski@gmail.com>
2024-11-28 19:22:39 +0500

    task7

---
+++ Tyuiu.KimmelDS.Sprint2.Task7.V9.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KimmelDS.Sprint2.Task7.V9.Lib
+{
+    public class DataService : ISprint2Task7V9
+    {
+        public bool CheckDotInShadedArea(double x, double y) => y <= Math.Sin(x) && y >= 0.5 ? true : false;
+        
+    }
+}
