86a5dbf1f69ba380dc177ae22dc7a4417b2abb37
EvilHui <Ukrumoski@gmail.com>
2024-11-28 19:00:52 +0500

    task4

---
+++ Tyuiu.KimmelDS.Sprint2.Task4.V30/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint2.Task4.V30.Lib;
+namespace Tyuiu.KimmelDS.Sprint2.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(2.133333, 2.53231);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
